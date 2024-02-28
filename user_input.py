f1b97be20fba9bb1d8a63a5910520bd0c0f9feff
Monyaluoe Nthabiseng Mphaka <catherinemphaka@gmail.com>
2024-02-29 01:25:46 +0200

    First commit to PLP python

---
+++ user_input.py
@@ -0,0 +1,14 @@
+#use the input() function to ask the user for their name and store it
+#in a variable called name.
+name=input("Enter your name: ")
+#use the input() function to ask the user for their age and store it
+#in a variable called age.
+age=input("Enter your age: ")
+#use the input()function to ask the user for their location and store it
+#in a variable called location.
+location=input("Enter your location: ")
+#print out a personalized message using the users name, age and location
+print("Hello,",name,"you are", age, "years old and you live in",location)
+
+
+
